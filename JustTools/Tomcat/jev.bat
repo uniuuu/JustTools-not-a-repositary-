74556b80bbfb5b257e9ea2c9bdd34c75d9a6ffe6
Admin <Admin@192.168.2.131>
2017-03-12 11:44:38 +0300

    first

---
+++ JustTools/Tomcat/jev.bat
@@ -0,0 +1,13 @@
+@echo off
+::::::::::::::::::::::::::::::::::::
+::  Set JAVA_HOME and JRE_HOME  ::
+::::::::::::::::::::::::::::::::::::
+
+:: set CLASSPATH =C:\xampp\Tomcat\lib\servlet-api.jar;C:\xampp\Tomcat\lib\jsp-api.jar
+:: set PATH=%PATH%;c:\Program Files\Java\jdk1.8.0_121\bin\;c:\xampp\tomcat\bin\
+set JAVA_HOME=C:\Program Files\Java\jdk1.8.0_121
+set JRE_HOME=C:\Program Files\Java\jre1.8.0_121
+
+:END
+echo done.
+::pause
